eaca2394eaaa15dafcdc7764177974998f90c71b
Michael Cain <michael.m.cain.ii@gmail.com>
2020-11-21 11:17:56 -0500

    add tests

--- src/Input.js
+++ src/Input.js
@@ -1,7 +1,12 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-const Input = () => {
+const Input = ({ secretWord }) => {
   return <input />;
+};
+
+Input.propTypes = {
+  secretWord: PropTypes.string.isRequired,
 };
 
 export default Input;
--- src/Input.spec.js
+++ src/Input.spec.js
@@ -1,10 +1,15 @@
 import React from "react";
 import { render } from "@testing-library/react";
+import { checkProps } from "../test/testUtils";
 import Input from "./Input";
 
 describe("Component rendering", () => {
   it("renders the Input component", () => {
-    const { getByRole } = render(<Input />);
+    const { getByRole } = render(<Input secretWord="jawn" />);
     expect(getByRole("textbox")).toBeInTheDocument();
+  });
+
+  it("properly renders props", () => {
+    checkProps(Input, { secretWord: "party" });
   });
 });
